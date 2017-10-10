7397222489e3fbc07e746d0cd49e6e09f4900ae7
DoingLee <ldongy2000@foxmail.com>
2017-10-10 22:44:53 +0800

    modify

--- src/main/java/com/doing/ReadWriteLockDemo.java
+++ src/main/java/com/doing/ReadWriteLockDemo.java
@@ -1,20 +1,17 @@
 package com.doing;
 
-import com.sun.tools.jdi.InternalEventHandler;
-
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteLockDemo {
 
-    /********************************* 线程安全例子 ****************************/
+    /********************************* 读写锁例子（写-读阻塞、写-写阻塞、读-写阻塞、读-读不阻塞） ****************************/
 
     private ConcurrentCache concurrentCache = new ConcurrentCache();
 
-    public void testThreadSafe() {
+    public void testReadWriteLock() {
 
         for (int i = 0; i < 2; i++) {
             final int num = i;
@@ -99,45 +96,108 @@ public class ReadWriteLockDemo {
 
     }
 
-    /********************************* 线程不安全例子 ****************************/
+//    /********************************* 线程安全例子 ****************************/
+//
+//    private SafeCache safeCache = new SafeCache();
+//
+//    public void testSafeCache() {
+//        for (int i = 0 ; i < 10; i++) {
+//            final int num = i;
+//            Thread putThread = new Thread(new Runnable() {
+//                public void run() {
+//                    System.out.println("放入鸡蛋：" + num);
+//                    safeCache.put("num" + num, "鸡蛋" + num);
+//                }
+//            });
+//            Thread getThread = new Thread(new Runnable() {
+//                public void run() {
+//                    System.out.println("获取鸡蛋：" + safeCache.get("num" + num));
+//                }
+//            });
+//            putThread.start();
+//            getThread.start();
+//        }
+//    }
+//
+//
+//    public class SafeCache {
+//        private HashMap<String, Object> map = new HashMap<String, Object>();
+//
+//        private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock(false); //默认为false - 非公平锁
+//        private Lock r = readWriteLock.readLock();
+//        private Lock w = readWriteLock.writeLock();
+//
+//        public Object get(String key) {
+//            Object o;
+//
+//            r.lock();
+//            try {
+//                o = map.get(key);
+//            } finally {
+//                r.unlock();
+//            }
+//
+//            return o;
+//        }
+//
+//        public void put(String key, Object value) {
+//            w.lock();
+//            try {
+//                map.put(key, value);
+//            } finally {
+//                w.unlock();
+//            }
+//        }
+//
+//        public void clear() {
+//            w.lock();
+//            try {
+//                map.clear();
+//            } finally {
+//                w.unlock();
+//            }
+//        }
+//    }
 
-    private Cache cache = new Cache();
-
-    public void testThreadUnSafe() {
-        for (int i = 0 ; i < 10; i++) {
-            final int num = i;
-            Thread putThread = new Thread(new Runnable() {
-                public void run() {
-                    System.out.println("放入鸡蛋：" + num);
-                    cache.put("num" + num, "鸡蛋" + num);
-                }
-            });
-            Thread getThread = new Thread(new Runnable() {
-                public void run() {
-                    System.out.println("获取鸡蛋：" + cache.get("num" + num));
-                }
-            });
-            putThread.start();
-            getThread.start();
-        }
-    }
-
-
-    public class Cache {
-        private HashMap<String, Object> map = new HashMap<String, Object>();
-
-        public Object get(String key) {
-            return map.get(key);
-        }
-
-        public void put(String key, Object value) {
-            map.put(key, value);
-            return;
-        }
-
-        public void clear() {
-            map.clear();
-            return;
-        }
-    }
+//    /********************************* 线程不安全例子 ****************************/
+//
+//    private UnSafeCache unSafeCache = new UnSafeCache();
+//
+//    public void testUnSafeCache() {
+//        for (int i = 0 ; i < 10; i++) {
+//            final int num = i;
+//            Thread putThread = new Thread(new Runnable() {
+//                public void run() {
+//                    System.out.println("放入鸡蛋：" + num);
+//                    unSafeCache.put("num" + num, "鸡蛋" + num);
+//                }
+//            });
+//            Thread getThread = new Thread(new Runnable() {
+//                public void run() {
+//                    System.out.println("获取鸡蛋：" + unSafeCache.get("num" + num));
+//                }
+//            });
+//            putThread.start();
+//            getThread.start();
+//        }
+//    }
+//
+//
+//    public class UnSafeCache {
+//        private HashMap<String, Object> map = new HashMap<String, Object>();
+//
+//        public Object get(String key) {
+//            return map.get(key);
+//        }
+//
+//        public void put(String key, Object value) {
+//            map.put(key, value);
+//            return;
+//        }
+//
+//        public void clear() {
+//            map.clear();
+//            return;
+//        }
+//    }
 }
--- src/test/java/com/doing/ReadWriteLockDemoTest.java
+++ src/test/java/com/doing/ReadWriteLockDemoTest.java
@@ -4,17 +4,23 @@ import org.junit.Test;
 
 public class ReadWriteLockDemoTest {
     @Test
-    public void testThreadSave() throws Exception {
-        new ReadWriteLockDemo().testThreadSafe();
+    public void testReadWriteLock() throws Exception {
+        new ReadWriteLockDemo().testReadWriteLock();
 
         Thread.sleep(15000);
     }
 
-    @Test
-    public void testThreadUnSafe() throws Exception {
-        new ReadWriteLockDemo().testThreadUnSafe();
-
-        Thread.sleep(3000);
-    }
-
+//    @Test
+//    public void testSafeCache() throws Exception {
+//        new ReadWriteLockDemo().testSafeCache();
+//
+//        Thread.sleep(3000);
+//    }
+//
+//    @Test
+//    public void testUnSafeCache() throws Exception {
+//        new ReadWriteLockDemo().testUnSafeCache();
+//
+//        Thread.sleep(3000);
+//    }
 }
